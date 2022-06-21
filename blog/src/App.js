12e0284bf678503d296ba3aa0279ef90ef4ba41b
정민희 <dev.alsgml1640@gmail.com>
2022-06-21 23:38:06 +0900

    feat : 모달창 기능추가 및 클릭시 제목불러오기
    리액트 클론코딩 복습
    안보고 생각해서 해보기 연습

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,6 +5,8 @@ function App() {
   
   let [글제목, 글제목변경] = useState(['남자 코드 추천','강남 우동 맛집','파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
+  let [title,setTitle] = useState(0);
 
   return (
     <div className="App">
@@ -18,35 +20,28 @@ function App() {
         글제목변경(copy);
       }}>가나다순 정렬</button>
 
-      <button onClick={ ()=>{
-        const copy = [...글제목];
-        copy[0] = '여자 코드 추천';
-        글제목변경(copy);
-      }}>클릭!</button>
+    {
+      글제목.map((e,i)=>{
+        return (
+          <div className='list'>
+            <h4 onClick={()=>{setModal(true); setTitle(i)}}>{글제목[i]}</h4>
+            <p>6월 14일 발행</p>
+          </div>
+        )
+      })
+    }
 
-      <div className='list'>
-          <h4>{글제목[0]} <span onClick={()=>{ 따봉변경(따봉 + 1)} }>❤️</span> {따봉}</h4>
-          <p>6월 14일 발행</p>
-      </div>
-      <div className='list'>
-          <h4>{글제목[1]}</h4>
-          <p>6월 14일 발행</p>
-      </div>
-      <div className='list'>
-          <h4>{글제목[2]}</h4>
-          <p>6월 14일 발행</p>
-      </div>
-
-      <Modal />
+    { modal === true ? <Modal title={title} 글제목={글제목} /> : null}
+      
     </div>
   );
 }
 
 {/* 모달창 */}
-function Modal(){
+function Modal(props){
   return (
   <div className='modal'>
-    <h4>제목</h4>
+    <h4>{props.글제목[props.title]}</h4>
     <p>날짜</p>
     <p>상세내용</p>
   </div>
